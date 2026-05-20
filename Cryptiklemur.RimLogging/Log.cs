93fc30333effd60e76fddab0b4c0165f48c56606
Aaron Scherer <aequasi@gmail.com>
2026-05-20 15:38:07 -0500

    feat(core): add Log static class skeleton

---
+++ Cryptiklemur.RimLogging/Log.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Runtime.CompilerServices;
+
+namespace Cryptiklemur.RimLogging;
+
+/// <summary>Entry point for emitting log messages through the RimLogging framework.</summary>
+public static class Log
+{
+    /// <summary>Name of the default log channel used when no channel is specified.</summary>
+    public const string DefaultChannel = "default";
+}
